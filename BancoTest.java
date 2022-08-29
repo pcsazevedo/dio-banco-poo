91888e24f8d4cec296121fa7d474437cc24bd97b
paulo <paulocesarudi16@gmail.com>
2022-08-29 18:56:13 -0300

    Primeiro commit do projeto já concluido

---
+++ BancoTest.java
@@ -0,0 +1,18 @@
+public class BancoTest {
+    public static void main(String[] args) {
+        Cliente paulo = new Cliente();
+        paulo.setNome("Paulo");
+
+        Conta cc = new ContaCorrente(paulo);
+        Conta poupanca = new ContaPoupanca(paulo);
+
+        cc.depositar(100);
+
+        cc.transferir(100,poupanca);
+
+        cc.imprimirInfosComuns();
+        poupanca.imprimirExtrato();
+
+
+    }
+}
